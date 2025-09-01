b50f2257d9eacc23faca186d1a7acfe2da90cf74
SourceCode-code <siddhantsourceandcode@gmail.com>
2025-09-01 22:00:09 +0530

    terminal commands

--- Github_Sessions/03_terminal_commands.js
+++ Github_Sessions/03_terminal_commands.js
@@ -1,18 +1,18 @@
-// git terminal commands 
+// git terminal commands
 
 // git pull origin branchname --> this commands gets the lastest changes in your  repo is downloaded to your local
 
-// best practices --> 
-// always before srtaring the work hit in terminal 
-// git pull orgin main 
+// best practices -->
+// always before srtaring the work hit in terminal
+// git pull orgin main
 
-// git diff --> shows you the differenece between last commit and present changes 
-// git checkout filepath --> this restores your file to last commit 
-// git checkout -f --> restores all the files to the last commit 
+// git diff --> shows you the differenece between last commit and present changes
+// git checkout filepath --> this restores your file to last commit
+// git checkout -f --> restores all the files to the last commit
 //git status --> shows you the current status of your working directory
-// git log --> shows you the logs of the commits 
-//git log -p --> shows you only recentcommit and its details 
-//git log --oneline -> shows you the complied graph recent logs 
-// git stash  --> tempoaraliy removes the changes to allow you to work in a different file 
-// git stash apply  --> brings thoses changes back 
+//git log --> shows you the logs of the commits
+//git log -p --> shows you only recentcommit and its details
+//git log --oneline -> shows you the complied graph recent logs
+//git stash  --> tempoaraliy removes the changes to allow you to work in a different file
+//git stash apply  --> brings thoses changes back 
 
