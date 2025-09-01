8aae667450bdc84055a9824512a99ece252ff95b
SourceCode-code <siddhantsourceandcode@gmail.com>
2025-09-01 21:55:26 +0530

    terminal commands

--- Github_Sessions/03_terminal_commands.js
+++ Github_Sessions/03_terminal_commands.js
@@ -7,7 +7,6 @@
 // git pull orgin main 
 
 // git diff --> shows you the differenece between last commit and present changes 
-
 // git checkout filepath --> this restores your file to last commit 
 // git checkout -f --> restores all the files to the last commit 
 //git status --> shows you the current status of your working directory
