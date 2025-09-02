8a51ba5e164e22f9747a4c3a1ba680b7a0b0958d
SourceCode-code <siddhantsourceandcode@gmail.com>
2025-09-02 21:50:46 +0530

    remove changes 1

---
+++ Github_Sessions/05_intermidate_terminal_commands.js
@@ -0,0 +1,24 @@
+// ===============================
+// Git Terminal Commands - Notes (Intermediate)
+// ===============================
+
+//1 git commit -a -m "message" --> this command is a mixture of git add . and git commit -m "meassge" this will directly 
+// add the changes to the stagging area 
+
+//2 git reset filepath --> this command will unstage the file without discarding the changes 
+//3 git reset --> this command will unstage the  all file without discarding the changes 
+
+/**
+ * difference -->
+ * git checkout -f               --> restore file to last (latest) commit
+ * git stash                    -->temperoly removes the changes to allow to work on other file (you can again bring back the changes )
+ * git reset                    -->will not make any chnage in the file just will remove ths from stagging to local
+ * git reset --hard <commit id> --> this command will restore your file  content to a the specific commit 
+ */
+
+//4 git reset --hard <commit id> --> this command will restore your file to a the specific commit 
+
+// git revert <commit id >      --> this command will restore your file to a the specific commit and restore commit will become  your last commit
+
+
+//4 git reset --hard <commit> -- this command will restore your branch to a specific commit 
