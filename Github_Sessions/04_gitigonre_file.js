e93e7418ffb6af160154e8be41228c5f1962074c
SourceCode-code <siddhantsourceandcode@gmail.com>
2025-09-02 21:24:12 +0530

    adding file to igonre

---
+++ Github_Sessions/04_gitigonre_file.js
@@ -0,0 +1,8 @@
+// so git - github has a functionally which allows you to block the pushing of certain  files 
+
+//.gitignore -> this files allows you to store the path of the file which are to be igonre during pushging
+// whats the meaning of adding file to git igonre --> the files will be present to your local machine but will not be 
+//avaible on github
+
+//how to add 
+// how to know your files are added to git igonre 
