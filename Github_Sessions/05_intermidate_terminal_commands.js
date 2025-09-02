001ab1700b2e448aa6ad3f435d09c1dfc5c9f534
SourceCode-code <siddhantsourceandcode@gmail.com>
2025-09-02 21:38:08 +0530

    advance commands

---
+++ Github_Sessions/05_intermidate_terminal_commands.js
@@ -0,0 +1,18 @@
+// ===============================
+// Git Terminal Commands - Notes (Intermediate)
+// ===============================
+
+//1 git commit -a -m "message" --> this command is a mixture of git add . and git commit -m "meassge" this will directly 
+// add the changes to the stagging area 
+
+//2 git reset filepath --> this command will unstage the file without discarding the changes 
+//3 git reset --> this command will unstage the  all file without discarding the changes 
+
+/**
+ * git checkout -f  -->
+ * git stash        -->
+ * git reset        -->
+ * 
+ */
+
+
