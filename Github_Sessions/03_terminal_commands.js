55117ee607e508172e115f15fd14c6647ffeb29d
SourceCode-code <siddhantsourceandcode@gmail.com>
2025-09-02 20:13:11 +0530

    new chnages

---
+++ Github_Sessions/03_terminal_commands.js
@@ -0,0 +1,46 @@
+// ===============================
+// Git Terminal Commands - Notes
+// ===============================
+
+// --- Pulling Latest Changes ---
+ // git pull origin branchname
+ // -> Downloads the latest changes from remote repo into your local
+
+ // Best Practice:
+ // Always pull before starting work
+ // git pull origin main
+
+
+// --- Checking Differences ---
+ // git diff
+ // -> Shows the difference between the last commit and your current changes
+
+
+// --- Restoring Files ---
+ // git checkout <filepath>
+ // -> Restores a specific file to the last commit
+
+ // git checkout -f
+ // -> Restores ALL files to the last commit
+
+
+// --- Status and History ---
+ // git status
+ // -> Shows the current status of your working directory
+
+ // git log
+ // -> Shows the commit history (detailed logs)
+
+ // git log -p
+ // -> Shows the most recent commit with details
+
+ // git log --oneline
+ // -> Shows a compact version of the commit history
+
+
+// --- Stashing Changes ---
+ // git stash
+ // -> Temporarily removes your changes and saves it (without committing)  to allow you to work on another file 
+
+ // git stash apply
+ // -> Restores the stashed changes
