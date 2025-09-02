43bcbe61bd900f31df363562d7c49f308e08eb29
SourceCode-code <siddhantsourceandcode@gmail.com>
2025-09-02 21:27:58 +0530

    adding file to igonre

--- Github_Sessions/04_gitigonre_file.js
+++ Github_Sessions/04_gitigonre_file.js
@@ -4,5 +4,14 @@
 // whats the meaning of adding file to git igonre --> the files will be present to your local machine but will not be 
 //avaible on github
 
+
+// conditions --> 
+//1 file name should ".gitignore"
+//2 .gitignore file should be created at the root or at parent folder
+
+
 //how to add 
+// add the file path or file name to the .gitignore
+
 // how to know your files are added to git igonre 
+ // the file will be blacked out
