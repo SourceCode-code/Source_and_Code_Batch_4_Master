64cf75cdbbb3a3ff41954b66db31332f987c25c0
SourceCode-code <siddhantsourceandcode@gmail.com>
2025-09-04 21:25:35 +0530

    attendance

--- Github_Sessions/06_branches.js
+++ Github_Sessions/06_branches.js
@@ -29,8 +29,9 @@
 //git branch -a --> shows all the branches 
 
 // conditions to create a new branch 
-// 1 all your master code must be updated --> git pull origin main 
+// 1 all your master code must be updated -->  1)git fetch  ,2)git pull origin main 
 // 2 now you can create a new branch 
 
 // git pull origin main  --> downlaods all the updates to the local
-// git fetch             --> this gets the lastest data of all the branches 
+// git fetch             --> this gets the lastest data of all the branches this doesnt physciall downlaod 
+
