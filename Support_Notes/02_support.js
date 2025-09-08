78fb8e4a4e25acafde976f21f120a04a6636cbb2
SourceCode-code <siddhantsourceandcode@gmail.com>
2025-09-08 19:10:54 +0530

    support session for PR raise

--- Support_Notes/02_support.js
+++ Support_Notes/02_support.js
@@ -1,2 +1,18 @@
 // todays date 
 //02/09/2025
+
+// Todays date - taken by Amol
+/*
+PR = Pull request
+
+steps for raise a PR
+step 1- git ftech
+step 2 - git pull origin main 
+chnages and add some code
+step 3 = git add .
+step 4- git commit -m " Message"
+step 5 - git push origin your branch
+step 6 - go to the UI(Github) and riase a Pr with description  
+
+
+*/
