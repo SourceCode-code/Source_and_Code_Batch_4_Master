d63c9e4886e6834dace9456ff3c3bb15e9f50747
SourceCode-code <siddhantsourceandcode@gmail.com>
2025-09-09 19:37:50 +0530

    support session for Strings and folder structure

--- Support_Notes/02_support.js
+++ Support_Notes/02_support.js
@@ -16,3 +16,42 @@ step 6 - go to the UI(Github) and riase a Pr with description
 
 todays attendance - Rishi, Akash,Swapnil,chaitanya and ghanshyam
 */
+
+// Todays date - 09-09-2025 taken by Amol
+/*
+let emptySring=" "
+String = in between "", '' and ``
+
+
+todays attendance - Rishi, Akash,Swapnil,chaitanya and ghanshyam
+*/
+let myname = "Amolkantilaljadhav"
+console.log(myname.length)
+// indexes of amol Amol = A=0,m=1,0=2,l=3
+// index is nothing but length-1
+
+console.log(myname[1])
+console.log(myname[myname.length-17])
+
+let firsstname = "Amol"
+let middlename = "Katilal"
+let lastname = "JAdhav"
+let fullname= `${firsstname} ${middlename} ${lastname}`
+console.log(fullname)
+
+//t fullname = `${firstName} ${midddleName} ${lastName}`
+
+// 10. Create a string `quote = "Learning JavaScript is awesome!"`. Use the `.length` property to find the length of the string and the index of the character 'J'.
+let quote = "Learning JavaScript is awesome!"
+console.log(quote.indexOf("J"))
+
+// 14. Write a program to pick a random element from string "qwertyuiopasdfghjklzxcvbnm" and log the selected element.
+let num="qwertyuiopasdfghjklzxcvbnm" // we get a number here 
+// num.length
+// let check = Math.floor(Math.random()*num.length)
+// console.log(num[check])
+
+//15 write a program to generate a random odd number form 15-150
+//(max value-minvamlue)+minvalue // 45-90
+
+//todays attendace = rishi akash swapnil chaitanya adesh ghanshyam
