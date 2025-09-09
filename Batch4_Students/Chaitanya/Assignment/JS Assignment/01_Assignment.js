a8e97c56c4fb3ad105d3a377033dec4aefbe5dce
Chaitanya9408 <chaitanyakute.work@gmail.com>
2025-09-09 23:07:09 +0530

    folder restructuring

---
+++ Batch4_Students/Chaitanya/Assignment/JS Assignment/01_Assignment.js
@@ -0,0 +1,63 @@
+// Write a single line comment which says, comments can make code readable
+//comments can make code readable
+
+// Write another single comment which says, Welcome to 30DaysOfJavaScript
+//Welcome to 30DaysOfJavaScript
+
+// Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+/*
+comments can make code readable,
+easy to reuse and informative
+*/
+
+// Declare four variables without assigning values
+let m;
+let n;
+let o;
+let p;
+console.log (m);
+console.log (n);
+console.log (o);
+console.log (p);
+
+// Declare four variables with assigned values
+let a = 10;
+let b = 20;
+const pie = 3.14;
+const pakistan = "yedigand"
+console.log (a);
+console.log (b);
+console.log (pie);
+console.log (pakistan);
+
+
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+let firstname = "chaitanya";
+let lastname = "kute";
+let maritalStatus = "unmarried";
+let country = "India";
+let age = "26";
+console.log (firstname);
+console.log (lastname);
+console.log (maritalStatus);
+console.log (country);
+console.log (age);
+
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+let Fname = "chaitanya", Lname = "kute", Mstatus = "single", mAge ="26", Country="India";
+console.log (Fname);
+console.log (Lname);
+console.log (Mstatus);
+console.log (mAge);
+console.log (Country);
+
+// Declare two variables myAge and yourAge and assign them initial values and log to the console.
+let chaitanyaAge = "26";
+let siddhantAge = "26";
+console.log (chaitanyaAge);
+console.log (siddhantAge);
+
+// Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+// Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
