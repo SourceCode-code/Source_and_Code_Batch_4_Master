ce7752d16452b4eef30df7718454df5268af7f91
Chaitanya9408 <chaitanyakute.work@gmail.com>
2025-09-12 19:34:39 +0530

    for todays session

--- Batch4_Students/Chaitanya/variable.js
+++
@@ -1,9 +0,0 @@
-
-// Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
-
-let name = "chaitanya";
-let iAmStudent = true;
-let x;
-let empty = null;
-
-console.log(name, iAmStudent, x, empty);
