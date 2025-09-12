cd0a9a09eb5dbd4a098f8e3e42ef7f6adf755cb9
SourceCode-code <siddhantsourceandcode@gmail.com>
2025-09-12 21:31:02 +0530

    methods for string

--- JavaScript_Sessions/Js_lectures/04_Datatypes_String_Method.js
+++ JavaScript_Sessions/Js_lectures/04_Datatypes_String_Method.js
@@ -1,7 +1,11 @@
 // Strings methods
 
+// String is a immutable datatype --> there can be now change in the string properties 
+
 // .toUpperCase() --> this method is used to convert string into upper case 
 let str = "Siddhant Arjun Gadakh"
+
+
 
 // the way to use any method on string is 
 
@@ -32,5 +36,41 @@ console.log(typeof (str3.length))
 
 // note every method has something called as output as well as return type 
 
+// Substr()    -->Substr is method which get a part of string (starting index,numbr of characters )
+let str4 = "Siddhant Arjun Gadakh"
+let newstr1 = str4.substr(0, 8)
+console.log(newstr1)
 
+// substring   -->subString is method which get a part of string (starting index,ending index) (it will not include the 
+// ending index)
 
+// this will help to retrun certain part of string 
+
+let newstr2 = str4.substring(0, 7)
+console.log(newstr2)
+
+// what is difference between substr and substring
+// the use of both the methods are one and the same it gets us a part of the string but the difference is the syntax
+// substr () --uses (starting index,number of characters )
+// subString()-- uses (starting index,ending index) (it will not include the ending index in the output)
+
+// split() --> this method is used to convert one string into many  new string --> this return an array
+
+//let str4 = "Siddhant Arjun Gadakh"
+ 
+
+console.log(str4.split(""))
+
+// //[
+//   'S', 'i', 'd', 'd', 'h',
+//   'a', 'n', 't', ' ', 'A',
+//   'r', 'j', 'u', 'n', ' ',
+//   'G', 'a', 'd', 'a', 'k',
+//   'h'
+// ]
+
+console.log(str4.split(" ")) //[ 'Siddhant', 'Arjun', 'Gadakh' ]
+
+console.log(str.split("a")) //[ 'Siddh', 'nt Arjun G', 'd', 'kh' ]
+
+//
