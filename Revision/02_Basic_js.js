24c4fcbc21fe13937938e557cd1fd2564198c326
SourceCode-code <siddhantsourceandcode@gmail.com>
2025-09-22 22:00:08 +0530

    update

---
+++ Revision/02_Basic_js.js
@@ -0,0 +1,69 @@
+
+// JAVASCRIPT --> Js is interactive and new gen deveoplment langauge used to create dynamic web application (webiste)
+
+
+// HTML --> (hypertext markup langauge ) --> was used to only display text and provide a stucture
+
+// CSS --> Cascading style sheet  --> css was for style the webpage  
+
+//JAVASCRIPT -> ES6 --> this was used to make your interactive 
+ 
+
+//------------------------------------------------------------------//
+
+// why to learn js as automation testor ?
+
+// you need js to create logic in the automation  for test scripts 
+// remember your js concepts will make you a better automation testor 
+
+
+// ------------------------------------------------------------------------------
+
+// Basic of JS 
+
+// when is one file consider js --> filename.js
+
+// to print anything ( logging ) 
+
+// this code is used to print hello world 
+console.log("hello  world ")
+
+// note always make sure to save your file first 
+// if you want to print anything is vs code in terminal 
+// 1 right click on the file and select open intergrated terminal
+//2 in terminal type node filename.js
+
+// comments -->  comments are the line which are used to describe and line will not affect code 
+
+//why ?
+// comments are used to decribe the code writteen
+// its the understanding of the code easy 
+// its makes the code maintable 
+
+
+// single line  comments --> the comment which is used to decirbe a code in single is called single comment 
+//  it is given by (//)
+
+// welcome to source and code 
+
+// multi line comments --> the comment which is used to decirbe a code in multi is called multi comment 
+
+// its denoted by (/*     jb    */)
+
+
+/* hello 
+this 
+is 
+source
+and code 
+*/
+
+//--------------------
+// there are some rules you need follow 
+
+//1 comments are used to explain the complex logics 
+//2  avoid obvious comments that do not add value  
+//3  keep comments updated with code changes 
+//4 comments should be clear and precise
+
+
