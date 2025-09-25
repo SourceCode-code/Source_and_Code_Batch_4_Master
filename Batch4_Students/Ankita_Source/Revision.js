4828e946476377e91174b8dbe88e10e1d1210889
Ankita Fartade <ankitafartde.work@gmail.com>
2025-09-25 16:56:29 +0530

    Assignment No_2 Completed

---
+++ Batch4_Students/Ankita_Source/Revision.js
@@ -0,0 +1,44 @@
+//Math.floor(Math.random()*(max-min +1))+min
+
+//let random1= Math.floor(Math.random()*(10-1+1))+1
+//console.log("random1=",random1);
+
+//let num = 5.7;
+//console.log("num=", Math.ceil(num));
+//console.log("num=", Math.floor(num));
+
+// formula Math.floor(Math.random()*(max-min +1))+min
+//1=min & 6=max
+
+//let dice1= Math.floor(Math.random()*(6-1+1))+1
+//let dice2= Math.floor(Math.random()*(6-1+1))+1
+//let total = (dice1+dice2);
+
+//console.log("dice1=",dice1);
+//console.log("dice2=",dice2);
+//console.log("total=", total);
+
+
+//let randomVal = Math.random();
+//console.log(Math.floor(randomVal * 10) + 5);
+
+let min =5.5;
+let max= 9.3;
+let randomVal= Math.random()*(9.3-5.5+1)+5.5;
+console.log("Number=", randomVal.toFixed(2));
+//if want last 2 digits only after decimal number use .toFixed()
+
+
+//let min1 = 5.5;
+//let max1= 9.3;
+//let randomFloat = Math.random()*(9.3-5.5)+5.5;
+//console.log("randomFloat=", randomFloat);
+
+// 9. Given a number `x = 1234.56789`, write code to round this number to 2 decimal places. that is x=1234.57
+//let x = 1234.56789;
+ //x= Number (x.toFixed(2));
+ //console.log("fixedNum=", x);
+
+    //let number=28
+   //let roundedNum= Math.round(numbern/5)*5
+    //console.log ("roundedNum=", roundedNum);
