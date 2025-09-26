08ea56b1d1f387bf81a58aeb0092e17d6de151d3
indrayani <indrayanisuryawanshi.work@gmail.com>
2025-09-26 19:49:22 +0530

    assingnment 01 completed

--- Batch4_Students/Indrayani/Assignment/JS_Assignment/Js_Assignments/01_Assigment _indrayani.js
+++ Batch4_Students/Indrayani/Assignment/JS_Assignment/Js_Assignments/01_Assigment _indrayani.js
@@ -3,46 +3,50 @@
 //comments can make code readable
 
 // Write another single comment which says, Welcome to 30DaysOfJavaScript
-//WE
+
+//Welcome to 30Daysofjavascript
 
 // Write a multiline comment which says, comments can make code readable, easy to reuse and informative
-/* comments can make code readable,
-   easy to reuse and informative */
+
+/*comments can make code readable,
+easy to reuse and informative*/
 
 // Declare four variables without assigning values
+
 let a
 let b
 let c
+
 // Declare four variables with assigned value
-let d = "rr"
-let e = "ok"
-let f = "hi"
-let g = "nice"
+
+let v = "good"
+let g = "best"
+let i = "hello"
+let p = "rr"
+
 // Declare variables to store your first name, last name, marital status, country and age in multiple lines
 
-let firstName = 'Adesh';
-let lastName = 'Devkar';
-let maritalStatus = 'Single';
+let firstName = 'Indrayani';
+let lastName = 'Suryawanshi';
+let maritalStatus = 'Married';
 let country = 'India';
-let age = 23;
+let age = 34;
 // Declare variables to store your first name, last name, marital status, country and age in a single line
-let fName = 'Adesh', lName = 'Devkar', maritalstatus = 'Single', nation = 'India', Age = 23;
+let fName = 'Indrayani',lName = 'Suryawanshi',maritalstatus = 'Married', nation = 'India', Age = 34;
 
 // Declare two variables myAge and yourAge and assign them initial values and log to the console.
 
-let myAge = 23;
-let yourAge = 30;
+let myAge = 34;
+let yourAge = 40;
 console.log(myAge);
 console.log(yourAge);
 
 // Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
-let str = 'hello world';
-let boolean = false;
+
+let str = 'hello world 1';
+let boolean = true;
 let undefined;
 let nullvar = null;
-
-
-
 
 // Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
 console.log(typeof str);
