e2a2d40062244d6a0c8ccab592ba18843bcb3ab0
Aakashjadhav86 <aakashjadhav.work@gmail>
2025-10-07 21:34:15 +0530

    to get latest pull

--- Batch4_Students/Aakash/Assignments/JS_Assignments/06_Assignment.js
+++ Batch4_Students/Aakash/Assignments/JS_Assignments/06_Assignment.js
@@ -1,1 +1,79 @@
-//will start soon
+// find out length of given string 
+let str1 = "repo batch source_and_Code_Batch_4_Master> git pull origin main"
+console.log(str1.length)
+let str2 = "These questions range from basic to advanced levels, helping you build a strong foundation and tackle complex problems involving\
+ strings. Let me know if you'd like solutions or further explanations for any of these! "
+ console.log(str2.length)
+
+let str3 = "1234!@#$%"
+console.log(str3.length)
+let str4 = "rupsssssssss"
+console.log(str4.length)
+let str5 = "qwertyuiopasdfghjklzxcvbnm"
+console.log(str5.length)
+
+//find out the index of given string
+let str6 = "repo batch source_and_Code_Batch_4_Master> git pull origin main"
+console.log(str6.indexOf("r"))
+console.log(str6.lastIndexOf("r"))
+//for last index
+console.log(str6[str6.length-1])
+
+let str7 = "These questions range from basic to advanced levels, helping you build a strong foundation and tackle complex problems involving\
+ strings. Let me know if you'd like solutions or further explanations for any of these! "
+ console.log(str7.indexOf("a"))
+ console.log(str7.lastIndexOf("a"))
+ console.log(str7[str7.length-1])
+
+let str8 = "1234!@#$%"
+console.log(str8.length) 
+console.log(str8.indexOf("!"))
+console.log(str8.lastIndexOf("%"))
+console.log(str8[str8.length-1])
+
+let str9 = "rupsssssssss"
+console.log(str4.length)
+
+console.log(str9.indexOf("s"))
+console.log(str9.lastIndexOf("s"))
+console.log(str9[str9.length-1])
+
+let str10 = "qwertyuiopasdfghjklzxcvbnm"
+console.log(str10.length)
+console.log(str10.indexOf("u"))
+console.log(str10.lastIndexOf("a"))
+console.log(str10[str10.length-1])
+
+//five examples of String concatenation:
+//using + operator and es6 string literal/literal template
+let str11 = "akash"
+let str12 = "amol"
+let str13 = "swapnil"
+console.log(str11 + " " + str12 + " " + str13)
+console.log(`${str11} ${str12} ${str13}`)
+
+let str14 = "uddhav"
+let str15 = "rushi"
+let str16 = "yogya"
+console.log(str14 + " " + str15 + " " + str16)
+console.log(`${str14} ${str15} ${str16}`)
+
+let str17 = "12345"
+let str18 = "!@#$"
+let str19 = "sdfg"
+console.log(str17 + " " + str18 + " " + str19)
+console.log(`${str17} ${str18} ${str19}`)
+
+let str20 = "gaurav kuldharan lhaapshi paav lover"
+let str21 = "guligat dhoka"
+let str22 = "amit shah chhachu"
+console.log(str20 + " " + str21 + " " + str22)
+console.log(`${str20} ${str21} ${str22}`)
+
+let str23 = "++++++++++++++"
+let str24 = "____________"
+let str25 = "***************"
+console.log(str23 + " " + str24 + " " + str25)
+console.log(`${str23} ${str24} ${str25}`)
+
+//
