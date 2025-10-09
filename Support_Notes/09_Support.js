845b9634887f62261e3c941971b6185a8f0843c0
SourceCode-code <siddhantsourceandcode@gmail.com>
2025-10-09 20:25:46 +0530

    support session

--- Support_Notes/09_Support.js
+++ Support_Notes/09_Support.js
@@ -7,3 +7,26 @@ let oddOreven = (randomNo%2 == 0)? `${randomNo} is a even number`:`${randomNo} i
 console.log(oddOreven);
 
 
+// 18. Calculate the slope, x-intercept, and y-intercept of y = 2x - 2.
+
+// let y = '2x - 2'
+// // y = 2X-2  -- X intercept
+// // x = 2y-2 -- y intercept
+// //y=mx+c  -- Slope
+
+// // Slope = 
+// let slope = y[0]
+// console.log(` slope is ${slope}`);
+// // X intercept
+// let XIntercept = 
+
+// 14. Write a program to pick a random element from string "qwertyuiopasdfghjklzxcvbnm" and log the selected element.
+
+let str = 'qwertyuiopasdfghjklzxcvbnm'
+
+console.log(str[Math.round(Math.random()* str.length-1)]);
+
+
+let randomNo1 = Math.round(Math.random()*(100-50)+50)
+console.log(randomNo1)
+
