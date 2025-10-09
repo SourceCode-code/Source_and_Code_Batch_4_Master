13ec1b819c2348384dd8c96a4692940258f840b6
Ashutosh more <ashutosh.more@expleogroup.com>
2025-10-09 18:40:06 +0530

    Added

---
+++ Support_Notes/08_support.js
@@ -0,0 +1,7 @@
+// date 09/10/2025
+// // Q36. Generate a random number between 1 and 100 and check if it is even or odd.
+// ternory operator
+let randomNo = Math.round(Math.random() * 100)
+
+(randomNo%2 == 1)?console.log(`${randomNo} is a even`):console.log(`${randomNo} is a odd`);
+
