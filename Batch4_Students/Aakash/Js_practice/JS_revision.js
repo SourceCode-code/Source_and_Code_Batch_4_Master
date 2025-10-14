b8f45df2493d164a02eff92073cfa502b9ee6bf3
Aakashjadhav86 <aakashjadhav.work@gmail>
2025-10-14 10:48:36 +0530

    to get latest pull

---
+++ Batch4_Students/Aakash/Js_practice/JS_revision.js
@@ -0,0 +1,9 @@
+let date = new Date
+let todayDate = date.getDate()
+console.log(todayDate)
+let currentMonth = date.getMonth()
+console.log(currentMonth)
+let currentYear = date.getFullYear()
+console.log(currentYear)
+let dateInLine = `${todayDate}/${currentMonth}/${currentYear}`
+console.log(dateInLine)
