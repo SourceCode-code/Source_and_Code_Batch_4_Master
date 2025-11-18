d403a8f383b1b5f1ad9b424fb7ca0086691cd754
Chaitanya9408 <chaitanyakute.work@gmail.com>
2025-11-19 02:10:19 +0530

    update

--- Batch4_Students/Chaitanya/Assignment/JS Assignment/12_Assignment.js
+++ Batch4_Students/Chaitanya/Assignment/JS Assignment/12_Assignment.js
@@ -1,28 +1,5 @@
 // Exercise: Level 1
 
-const countries = [
-    "Albania",
-    "Bolivia",
-    "Canada",
-    "Denmark",
-    "Ethiopia",
-    "Finland",
-    "Germany",
-    "Hungary",
-    "Ireland",
-    "Japan",
-    "Kenya",
-];
-
-const webTechs = [
-    "HTML",
-    "CSS",
-    "JavaScript",
-    "React",
-    "Redux",
-    "Node",
-    "MongoDB",
-];
 
 // Declare an empty array
 let a = [];
@@ -88,6 +65,10 @@ else {
 
 // Filter out companies which have more than one 'o' without using the filter method
 
+
+
+
+
 // Sort the array using sort() method
 
 let sortarray = companies.sort()
@@ -131,53 +112,140 @@ delete companies [2]
 
 // First remove all the punctuations and change the string to an array and count the number of words in the array:
  let text = 'I love teaching and empowering people. I teach HTML, CSS, JS, React, Python.'
+ let words = text.split(" ")
+ console.log(words)
+ console.log(words.length)
 // console.log(words)
 // console.log(words.length)
+
+
+
+
+
+
+
+
 
 
 // ["I", "love", "teaching", "and", "empowering", "people", "I", "teach", "HTML", "CSS", "JS", "React", "Python"]
 // 13
 
 // In the following shopping cart add, remove, edit items:
-// const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
+ const shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
+ shoppingCart.splice(1,0,"Peanuts")  // added 1 item in the shopping cart
+console.log(shoppingCart)
+
+shoppingCart.splice(0,1)           // removes an item from whichever position we want
+console.log(shoppingCart)
+
 // add 'Meat' in the beginning of your shopping cart if it has not been already added
+shoppingCart.splice(0,1,"Maeat")
+console.log(shoppingCart)
+console.log(shoppingCart.length)
+
 // add Sugar at the end of your shopping cart if it has not been already added
+shoppingCart.splice(5,0,"sugar")
+console.log(shoppingCart)
+
 // remove 'Honey' if you are allergic to honey
+console.log(shoppingCart.indexOf("honey"))
+shoppingCart.splice(3,1)
+console.log(shoppingCart)
+
 // modify 'Tea' to 'Green Tea'
+shoppingCart[2] = "Green Tea"
+console.log(shoppingCart)
 
 // In countries array check if 'Ethiopia' exists in the array, if it exists print 'ETHIOPIA'. If it does not exist add to the countries list.
+const countries = [
+    "Albania",
+    "Bolivia",
+    "Canada",
+    "Denmark",
+    "Ethiopia",
+    "Finland",
+    "Germany",
+    "Hungary",
+    "Ireland",
+    "Japan",
+    "Kenya",
+];
+
+if (countries.includes("Ethiopia")){
+console.log("ETHIOPIA")
+}
+else {
+    console.log("Ethiopia")
+}
 // In the webTechs array check if 'Sass' exists in the array and if it exists print 'Sass is a CSS preprocess'. If it does not exist, add Sass to the array and print the array.
 
+const webTechs = [
+    "HTML",
+    "CSS",
+    "JavaScript",
+    "React",
+    "Redux",
+    "Node",
+    "MongoDB",
+];
+
+if (webTechs.includes('Sass')) {
+console.log('Sass is a CSS preprocess')
+}
+else {
+    webTechs.unshift("Sass")
+    console.log(webTechs)
+}
 // Concatenate the following two variables and store it in a fullStack variable:
-// const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
-// const backEnd = ['Node', 'Express', 'MongoDB']
-// console.log(fullStack)
+const frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+ const backEnd = ['Node', 'Express', 'MongoDB']
+ let fullStack = frontEnd.concat(backEnd)
+ console.log(fullStack)
+ 
 // ["HTML", "CSS", "JS", "React", "Redux", "Node", "Express", "MongoDB"]
 
 // Exercise: Level 3
 
 // The following is an array of 10 students' ages:
 // Sort the array and find the min and max age
+
+const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
+let newAge = ages.sort((a,b) => {
+    return (a-b)
+})
+console.log(newAge)
+console.log(Math.max(...newAge))
+console.log(Math.min(...newAge))
+
 // Find the median age(one middle item or two middle items divided by two)
+
+
 // Find the average age(all items divided by number of items)
+    let sum = 0 
+    for (let i = 0 ; i < ages.length ; i++){
+        sum = sum + ages[i]
+    }
+console.log(sum/ages.length)
+
+
 // Find the range of the ages (max minus min)
 // Compare the value of (min - average) and (max - average), use abs() method
+
 // Slice the first ten countries from the countries array
+const countries1 = [
+    "Albania",
+    "Bolivia",
+    "Canada",
+    "Denmark",
+    "Ethiopia",
+    "Finland",
+    "Germany",
+    "Hungary",
+    "Ireland",
+    "Japan",
+    "Kenya",
+];
 // Find the middle country(ies) in the countries array
 // Divide the countries array into two equal arrays. If the countries array is not even, add one more country to the first half.
 
-// The following is an array of 10 students' ages:
-// const ages = [19, 22, 19, 24, 20, 25, 26, 24, 25, 24]
-// Sort the array and find the min and max age
-// Find the median age(one middle item or two middle items divided by two)
-// Find the average age(all items divided by number of items)
-// Find the range of the ages (max minus min)
-// Compare the value of (min - average) and (max - average), use abs() method
 
-// Slice the first ten countries from the countries array
-// Find the middle country(ies) in the countries array
-// Divide the countries array into two equal arrays. If the countries array is not even, add one more country to the first half.
-
-
-console.log('hello world')
-console.log('error')
