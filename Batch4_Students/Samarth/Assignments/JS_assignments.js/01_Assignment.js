8cc76577035ee305f73eac37bb33788e6224511e
Samarth-J09 <Jsamarth09.work@outlook.com>
2025-11-20 02:22:14 +0530

    new

---
+++ Batch4_Students/Samarth/Assignments/JS_assignments.js/01_Assignment.js
@@ -0,0 +1,65 @@
+//Write a single line comment which says, comments can make code readable
+
+// Readable code
+
+// Write another single comment which says, Welcome to 30DaysOfJavaScript
+
+// Javascriptfor 30days
+
+
+// Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+
+/* comment can make code readable
+ easy to refuse and informative */
+
+
+
+// Declare four variables without assigning values
+
+let Nation
+let capital
+var market
+// const distance
+
+
+
+
+
+// Declare four variables with assigned value
+
+//let Nation = Bharat
+//let capital = Maharastra
+//var market = fluctuates
+//const distance = Space travel
+
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+/* let name = Samarth
+   let surname = Jadhav
+   let Marrage = Unmarried
+   let country = Bharat
+   let Age =26 */
+
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+
+// let name = Samarth, let lastname = Jadhav, let marriage = unmarried, let country = Bharat, let age = 26
+
+
+// Declare two variables myAge and yourAge and assign them initial values and log to the console.
+
+let myage = 9
+let yourage = 9
+
+console.log(myage)
+
+
+
+
+
+// Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+
+
+
+// Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
