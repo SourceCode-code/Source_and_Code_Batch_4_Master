5a47f554a3b6428cd715df35e981e2f7ea65d0f4
Aakashjadhav86 <aakashjadhav.work@gmail>
2025-11-20 10:48:18 +0530

    to get latest pull

--- JavaScript_Sessions/Js_Assignments/12_Assignment.js
+++ JavaScript_Sessions/Js_Assignments/12_Assignment.js
@@ -4,60 +4,65 @@
 // Question 1: Skills and Points Analysis
 // Given the following users object:
 
-// const users = {  
-//   Alex: {
-//     email: 'alex@alex.com',
-//     skills: ['HTML', 'CSS', 'JavaScript'],
-//     age: 20,
-//     isLoggedIn: false,
-//     points: 30
-//   },
-//   Asab: {
-//     email: 'asab@asab.com',
-//     skills: ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
-//     age: 25,
-//     isLoggedIn: false,
-//     points: 50
-//   },
-//   Brook: {
-//     email: 'daniel@daniel.com',
-//     skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
-//     age: 30,
-//     isLoggedIn: true,
-//     points: 50
-//   },
-//   Daniel: {
-//     email: 'daniel@alex.com',
-//     skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
-//     age: 20,
-//     isLoggedIn: false,
-//     points: 40
-//   },
-//   John: {
-//     email: 'john@john.com',
-//     skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
-//     age: 20,
-//     isLoggedIn: true,
-//     points: 50
-//   },
-//   Thomas: {
-//     email: 'thomas@thomas.com',
-//     skills: ['HTML', 'CSS', 'JavaScript', 'React'],
-//     age: 20,
-//     isLoggedIn: false,
-//     points: 40
-//   },
-//   Paul: {
-//     email: 'paul@paul.com',
-//     skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
-//     age: 20,
-//     isLoggedIn: false,
-//     points: 40
-//   }
-// };
+const users = {  
+  Alex: {
+    email: 'alex@alex.com',
+    skills: ['HTML', 'CSS', 'JavaScript'],
+    age: 20,
+    isLoggedIn: false,
+    points: 30
+  },
+  Asab: {
+    email: 'asab@asab.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
+    age: 25,
+    isLoggedIn: false,
+    points: 50
+  },
+  Brook: {
+    email: 'daniel@daniel.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+    age: 30,
+    isLoggedIn: true,
+    points: 50
+  },
+  Daniel: {
+    email: 'daniel@alex.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+    age: 20,
+    isLoggedIn: false,
+    points: 40
+  },
+  John: {
+    email: 'john@john.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+    age: 20,
+    isLoggedIn: true,
+    points: 50
+  },
+  Thomas: {
+    email: 'thomas@thomas.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+    age: 20,
+    isLoggedIn: false,
+    points: 40
+  },
+  Paul: {
+    email: 'paul@paul.com',
+    skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
+    age: 20,
+    isLoggedIn: false,
+    points: 40
+  }
+};
 
 // Tasks:
 // 1. Find the person with the most skills.
+console.log(users[skills.length])
+
+users.forEach((el,index,arr)=>{
+    users.skills.length
+})
 // 2. Count the number of users who are logged in.
 // 3. Count the number of users with points greater than or equal to 50.
 // 4. Identify the MERN stack developers in the users object.
