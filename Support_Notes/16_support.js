fd36e825ea5cb3891448d036813d0c7cc3447459
SourceCode-code <siddhantsourceandcode@gmail.com>
2025-11-26 20:14:25 +0530

    support session

---
+++ Support_Notes/16_support.js
@@ -0,0 +1,30 @@
+// without parameter without return type
+function Calculator1(){
+    console.log(10+10);
+    
+}
+// with parameter without return type
+function calculaotr2(a,b){
+    console.log(a+b);
+    
+
+}
+// Function with parameter and with return type
+
+function Calculator3(a,b){
+    return a+b
+
+}
+Calculator1()
+calculaotr2(10,10)
+console.log(Calculator3(10,10))
+//Map,reduce,filter---
+
+// How to raise jira ticket 
+// expected output not equal to actual output the you have to raise the bug on jira
+//steps - 1. click on create button create
+//2. select bug
+//3. attach a epic link
+//4.assign to the developer
+// 5. In discription Add the steps to reproduce the bug and attach the screenshot of actual result and expected result
+// 6. mention the proivority and seviority 
