b222fc214428ba27aa2de65bd9cfea1dc2fff2b6
SourceCode-code <siddhantsourceandcode@gmail.com>
2025-12-10 21:35:14 +0530

    updates

--- JavaScript_Sessions/Js_lectures/20_High_order_functions.js
+++ JavaScript_Sessions/Js_lectures/20_High_order_functions.js
@@ -34,7 +34,82 @@ const higherorder = (n) => {
 
 console.log(higherorder(2)(3)(5));
 
-// there are 2 method  
+// there are 2 method
 // 1 setInterval()
 // 2 setTimeout()
 
+//setInterval()---> the gobal method which takes a callback function as parameter and duration as a paramter
+
+// function of this method --> this will trigger the function at given interval of time
+
+//sytnax
+
+/**
+ *
+ * function callback(){
+ * //code
+ * }
+ *
+ *
+ * setInterval(callback(),timeInterval ) the timeINvertal here will given in mili seconds
+ *
+ *
+ *
+ */
+
+// function SayHello(){
+//   console.log("hello")
+// }
+
+// setInterval(SayHello,2000)
+
+// 2 setTimeout() --> this method is used to trigger a function by given amount of delay
+
+function Saybye() {
+  console.log("bye");
+}
+
+setTimeout(Saybye, 5000);
+
+// hybrid problem
+
+let interval = setInterval(() => {
+  console.log("this will repeat after 3 secs");
+}, 3000);
+
+setTimeout(() => {
+  clearInterval(interval);
+  console.log("program has been ended after 10 secs");
+}, 10000);
+
+//-----------------------------------------------------------------------------------------------
+
+// Sync & Async
+
+// syncrous -->  your code will always be exceuted line by line
+
+console.log("hello");
+console.log("world");
+console.log("1111111111111");
+console.log(">>>>>>>>>>>>>>>>>>>");
+
+// is javascript by nature --> javascript is syncrous
+
+// Asyncorus --> code the which is easy to complice or which get complie first it gets to the out first
+
+// 1) callback hell
+//2) promises  --> cypress
+//3) async and await --> playwright
+
+// but your Automation tools by nature are async --> cypress , playwright
+
+// we will bulid our own small food delivery (zomato)
+
+/**Zomato flow 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
