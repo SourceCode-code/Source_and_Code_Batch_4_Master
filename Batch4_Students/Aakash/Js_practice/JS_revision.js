4c5fc6865f70d9fc399c96ee44aee65c11f60285
Aakashjadhav86 <aakashjadhav.work@gmail>
2025-12-15 20:32:51 +0530

    to get latest pull

--- Batch4_Students/Aakash/Js_practice/JS_revision.js
+++ Batch4_Students/Aakash/Js_practice/JS_revision.js
@@ -1,118 +1,158 @@
-// // 
+// // // // 
 
-// let rows = 4
-// for(let i =1 ;i<=4;i++){
-// console.log((String(i).repeat(rows--)))}
+// // // let rows = 4
+// // // for(let i =1 ;i<=4;i++){
+// // // console.log((String(i).repeat(rows--)))}
 
-// let str = " hello my name is siddhant gadakh and my age is 26 "
-// let tvowel = 0
+// // // let str = " hello my name is siddhant gadakh and my age is 26 "
+// // // let tvowel = 0
 
 
-// for(let i =0;i<str.length;i++){
-//   if(str[i]==="a"||str[i]==="e"||str[i]==="i"||str[i]==="o"||str[i]==="u"){
-//     tvowel++
-//   }
-// }
+// // // for(let i =0;i<str.length;i++){
+// // //   if(str[i]==="a"||str[i]==="e"||str[i]==="i"||str[i]==="o"||str[i]==="u"){
+// // //     tvowel++
+// // //   }
+// // // }
 
-// console.log(tvowel)
+// // // console.log(tvowel)
 
-let Companies10 = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"];
-let slicedAr1 = Companies10.splice(2,3)
-console.log(slicedAr1)
-let Companies12 = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"];
-Companies12.splice(0,Companies12.length)
-console.log("hh",Companies12)
+// // let Companies10 = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"];
+// // let slicedAr1 = Companies10.splice(2,3)
+// // console.log(slicedAr1)
+// // let Companies12 = ["Facebook", "Google", "Microsoft", "Apple", "IBM", "Oracle", "Amazon"];
+// // Companies12.splice(0,Companies12.length)
+// // console.log("hh",Companies12)
 
-let shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
-shoppingCart.push("Sugar")
-console.log(shoppingCart)
+// // let shoppingCart = ['Milk', 'Coffee', 'Tea', 'Honey']
+// // shoppingCart.push("Sugar")
+// // console.log(shoppingCart)
 
-shoppingCart.unshift("Meat")
-console.log(shoppingCart)
+// // shoppingCart.unshift("Meat")
+// // console.log(shoppingCart)
 
-shoppingCart.splice(3)
-console.log(shoppingCart)
+// // shoppingCart.splice(3)
+// // console.log(shoppingCart)
 
-let frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
-let backEnd = ['Node', 'Express', 'MongoDB']
-let fullStack = frontEnd.concat(backEnd)
-console.log(fullStack)
+// // let frontEnd = ['HTML', 'CSS', 'JS', 'React', 'Redux']
+// // let backEnd = ['Node', 'Express', 'MongoDB']
+// // let fullStack = frontEnd.concat(backEnd)
+// // console.log(fullStack)
 
-// let num3 = [2, 8, 7, 3, 35, 58, 69, 34567, 367, 2, 3, 4, 5, 67, 8, 9, 0]
+// // // let num3 = [2, 8, 7, 3, 35, 58, 69, 34567, 367, 2, 3, 4, 5, 67, 8, 9, 0]
 
-// let minage = Math.min(...ages)
-// console.log("age",minage)
-// let maxage = Math.max(...ages)
-// console.log(maxage)
+// // // let minage = Math.min(...ages)
+// // // console.log("age",minage)
+// // // let maxage = Math.max(...ages)
+// // // console.log(maxage)
 
-// let arr1 = [1, 2, 3];
-// let arr2 = [...arr1];
-// console.log(arr1)
+// // // let arr1 = [1, 2, 3];
+// // // let arr2 = [...arr1];
+// // // console.log(arr1)
 
-// let arr_11 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
-// let secondBig = arr_11.sort((a, b) => b - a)[1];
+// // // let arr_11 = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+// // // let secondBig = arr_11.sort((a, b) => b - a)[1];
 
-// console.log(secondBig);
+// // // console.log(secondBig);
 
-let users = {  
-  Alex: {
-    email: 'alex@alex.com',
-    skills: ['HTML', 'CSS', 'JavaScript'],
-    age: 20,
-    isLoggedIn: false,
-    points: 30
-  },
-  Asab: {
-    email: 'asab@asab.com',
-    skills: ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
-    age: 25,
-    isLoggedIn: false,
-    points: 50
-  },
-  Brook: {
-    email: 'daniel@daniel.com',
-    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
-    age: 30,
-    isLoggedIn: true,
-    points: 50
-  },
-  Daniel: {
-    email: 'daniel@alex.com',
-    skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
-    age: 20,
-    isLoggedIn: false,
-    points: 40
-  },
-  John: {
-    email: 'john@john.com',
-    skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
-    age: 20,
-    isLoggedIn: true,
-    points: 50
-  },
-  Thomas: {
-    email: 'thomas@thomas.com',
-    skills: ['HTML', 'CSS', 'JavaScript', 'React'],
-    age: 20,
-    isLoggedIn: false,
-    points: 40
-  },
-  Paul: {
-    email: 'paul@paul.com',
-    skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
-    age: 20,
-    isLoggedIn: false,
-    points: 40
-  }
-};
-// users.akash =  {
-//     email: 'paul@paul.com',
-//     skills: ['HTML', 'CSS', 'Node'],
-//     age: 20,
-//     isLoggedIn: false,
-//     points: 40
-//   }
-//   console.log(users)
+// // let users = {  
+// //   Alex: {
+// //     email: 'alex@alex.com',
+// //     skills: ['HTML', 'CSS', 'JavaScript'],
+// //     age: 20,
+// //     isLoggedIn: false,
+// //     points: 30
+// //   },
+// //   Asab: {
+// //     email: 'asab@asab.com',
+// //     skills: ['HTML', 'CSS', 'JavaScript', 'Redux', 'MongoDB', 'Express', 'React', 'Node'],
+// //     age: 25,
+// //     isLoggedIn: false,
+// //     points: 50
+// //   },
+// //   Brook: {
+// //     email: 'daniel@daniel.com',
+// //     skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux'],
+// //     age: 30,
+// //     isLoggedIn: true,
+// //     points: 50
+// //   },
+// //   Daniel: {
+// //     email: 'daniel@alex.com',
+// //     skills: ['HTML', 'CSS', 'JavaScript', 'Python'],
+// //     age: 20,
+// //     isLoggedIn: false,
+// //     points: 40
+// //   },
+// //   John: {
+// //     email: 'john@john.com',
+// //     skills: ['HTML', 'CSS', 'JavaScript', 'React', 'Redux', 'Node.js'],
+// //     age: 20,
+// //     isLoggedIn: true,
+// //     points: 50
+// //   },
+// //   Thomas: {
+// //     email: 'thomas@thomas.com',
+// //     skills: ['HTML', 'CSS', 'JavaScript', 'React'],
+// //     age: 20,
+// //     isLoggedIn: false,
+// //     points: 40
+// //   },
+// //   Paul: {
+// //     email: 'paul@paul.com',
+// //     skills: ['HTML', 'CSS', 'JavaScript', 'MongoDB', 'Express', 'React', 'Node'],
+// //     age: 20,
+// //     isLoggedIn: false,
+// //     points: 40
+// //   }
+// // };
+// // // users.akash =  {
+// // //     email: 'paul@paul.com',
+// // //     skills: ['HTML', 'CSS', 'Node'],
+// // //     age: 20,
+// // //     isLoggedIn: false,
+// // //     points: 40
+// // //   }
+// // //   console.log(users)
 
-let asusers = Object.assign({},users)
-console.log("kk",asusers)
+// // let asusers = Object.assign({},users)
+// // console.log("kk",asusers)
+
+
+// // var xx =10
+
+// // console.log(xx)
+
+// // function printxx(){
+// //     console.log(xx)
+// // }
+
+// // printxx()
+
+// // var xx = 10
+// // console.log(xx)
+// // function printxx(){
+// //   console.log(xx)
+// // }
+// // printxx()
+
+// // const pii = 3.1432;
+
+// // function printpi() {
+// //   const pii = 3.14;
+// //   console.log(pii);
+// // }
+
+// // console.log(pii);
+// // printpi();
+// // const pii = 3.1432
+// // function printpi(){
+// //   const pii = 3.14
+// //   console.log(pii)
+// // }
+
+// // console.log(pii)
+// // printpi()
+
+// let pattern = "siddhant";
+// let regex = new RegExp(pattern);
+// console.log(regex); // 
