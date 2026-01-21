7886d7f4f88a2dbe1ad3241bc8b286c056b89558
udaynpatil5353 <udaynpatil5353@gmail.com>
2026-01-21 08:17:53 +0530

    02_Assignment of Playwrite

--- students_playwright/tests/Uday/01_Assignment.spec.js
+++ students_playwright/tests/Uday/01_Assignment.spec.js
@@ -4,7 +4,7 @@ test("verify the basic of playwright ", async({browser})=>{
     const Context = await browser.newContext() 
     const page = await Context.newPage()
     
-    await page.goto("https://www.youtube.com/") 
+    await page.goto("https://www.youtube.com") 
     
     await expect(page).toHaveTitle("YouTube")
 })
