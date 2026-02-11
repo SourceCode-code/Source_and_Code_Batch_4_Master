c647d3e063684f088efa7c78c32a598edb3653ad
SourceCode-code <siddhantsourceandcode@gmail.com>
2026-02-11 20:25:31 +0530

    06 mutli window page

--- playwright_main/tests/09_mutlitabs.spec.js
+++ playwright_main/tests/09_mutlitabs.spec.js
@@ -38,3 +38,22 @@ test(" Validate the multi Window in playwright", async function ({ browser }) {
         "Lets Get Clicking!",
     );
 });
+
+
+
+
+test("verify Multiwindow handling", async function({browser}){
+
+    const context = await browser.newContext();
+    const page = await context.newPage();
+
+    await page.goto("https://vinothqaacademy.com/multiple-windows/");
+
+    const [newwindow] = await Promise.all([
+        context.waitForEvent("page"),
+        page.locator('[name="newbrowserwindow123"]').click()
+    ]);
+
+    await newwindow.locator('[placeholder="Name"]').fill("Uday");
+    await expect(newwindow.locator('[placeholder="Name"]')).toHaveValue("Uday");
+})
