31b399053878fcc95d97621270f16de3fa363f92
SourceCode-code <siddhantsourceandcode@gmail.com>
2026-02-20 21:29:07 +0530

    14 API

--- playwright_main/tests/14_API.spec.js
+++ playwright_main/tests/14_API.spec.js
@@ -6,7 +6,7 @@
 // BACKEND  --> (THE ACTION THAT YOU GET AFTER GIVEING REACTION TO WEB PAGE ) ---> API 
 // SERVER  --> DATA STORE PLACES 
 
-// TECHINACAL ROLES 
+// TECHINCAL ROLES 
 
 //1 FRONT END DEVEOPLER  --> HTML , CSS, JS ,
 //2 BACKEND DEVELOPER  --> 
@@ -30,6 +30,7 @@
 // REST SERVICES ARE TESTED ON TOOLS LIKE --> POSTMAN , SWAAGER 
 
 
+
 // REST SERVICES -->
 
 //GET --> IT GETS AALL THE INFORMATION --> 
@@ -42,3 +43,56 @@
 
 // IF YOU HAVE  A SINGLE API TO TEST --> API REQUEST 
 // IF YOU HAVE  A MULTIPLE API TO TEST --> COLLECTION   
+
+
+// SAMPLE OF XML DATA 
+
+/**
+<Colors>
+<Color1>White</Color1>
+<Color2>Blue</Color2>
+<Color3>Black</Color3>
+<Color4 Special="Light">Green</Color4>
+<Color5>Red</Color5>
+</Colors>
+<Fruits>
+<Fruits1>Apple</Fruits1>
+<Fruits2>Pineapple</Fruits2>
+<Fruits3>Grapes</Fruits3>
+<Fruits4>Melon</Fruits4>
+</Fruits>
+</SampleXML>'
+ * 
+ */
+
+
+// JSON SAMPLE
+
+/**
+ {
+  "Page_title": "WebDriver | Contact Us",
+  "section_header": "CONTACT US",
+  "firstName": "user 100",
+  "lastName": " data 1000",
+  "email": "userdata123@gmail.com",
+  "comment": "hello this is demo of testdata ",
+  "success_message": "Thank You for your Message!"
+}
+
+ */
+
+
+//SATUS --> CODE  --> SHOWS THE STATUS OF API --> 25 CODES 
+
+
+// WHEN YOU WHEN WEBISTE --> WWW.YOUTUBE.COM --> URL -> UNIQUE RESOUCRES LOCTOTR 
+
+// API URL 
+
+// BASEURL                             QUERY PARAMETER 
+              
+//https://www.youtube.com/           results?search_query=playwright
+
+                              // PATH PARAMTER
+
+//https://reqres.in           /api/test-suite/collections/users/records
