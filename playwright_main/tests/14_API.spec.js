78d92fe4b2db31a663c26eb1e143dd7a7692de0c
SourceCode-code <siddhantsourceandcode@gmail.com>
2026-02-26 21:25:09 +0530

    api pw

--- playwright_main/tests/14_API.spec.js
+++ playwright_main/tests/14_API.spec.js
@@ -90,17 +90,18 @@
 // API URL 
 
 // BASEURL                             QUERY PARAMETER 
-              
+
 //https://www.youtube.com/           results?search_query=playwright
 
-                              // PATH PARAMTER
+// PATH PARAMTER
 
 //https://reqres.in           /api/test-suite/collections/users/records
 
 
-const { test, expect,request } = require("@playwright/test")
+const { test, expect, request } = require("@playwright/test")
+const { exitCode } = require("process")
 
-test("verify GET API",async({request})=>{
+test("verify GET API", async ({ request }) => {
 
   let req = await request.get("https://jsonplaceholder.typicode.com/posts")
 
@@ -110,9 +111,47 @@ test("verify GET API",async({request})=>{
 
   let response = await req.json()
 
-   expect( req.status()).toBe(200)
+  expect(req.status()).toBe(200)
 
-   expect(response[0].title).toEqual("sunt aut facere repellat provident occaecati excepturi optio reprehenderit")
+  expect(response[0].title).toEqual("sunt aut facere repellat provident occaecati excepturi optio reprehenderit")
 
 
 })
+
+test("verify POST API ", async ({ request }) => {
+
+  let req = await request.post("https://jsonplaceholder.typicode.com/posts", {
+    data: {
+      "ID": "01000",
+      "title": "demo POST QUERTY",
+      "body": "THIS IS DEMO API TO CREATE A POST DATA ",
+      "REESUT TYPE ": "POST"
+    }
+  })
+  let responsepost = await req.json()
+  console.log(responsepost)
+  expect(req.status()).toBe(201)
+  expect(responsepost.title).toEqual("demo POST QUERTY")
+})
+
+
+//.toBeOK() a automatic aseeration for api success status code
+
+test("verify PUT API ", async ({ request }) => {
+
+  let req = await request.put("https://jsonplaceholder.typicode.com/posts/1", {
+    data: {
+      "ID": "01000",
+      "title": "demo POST QUERTY",
+      "body": "THIS IS DEMO API TO CREATE A POST DATA ",
+      "number":"987456"
+     
+    }
+  })
+  let responsepost = await req.json()
+  console.log(responsepost)
+  expect(req.status()).toBe(200)
+  expect(responsepost.number).toEqual("123456")
+})
+
+
