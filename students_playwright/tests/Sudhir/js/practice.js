249716990a04304e3597d5bfdc179f7f94371f25
SG-AILearner <gaikwadsudhir300@gmail.com>
2026-03-14 14:44:49 +0530

    new

---
+++ students_playwright/tests/Sudhir/js/practice.js
@@ -0,0 +1,31 @@
+// // change every second word to uppercase
+
+// //input = My Name is Sayali Sagar
+// //Output = mY nAme iS sAyali sAgar
+
+let input = 'Sudhir is my name'
+
+let wordArray = input.split(" ")
+
+let newword = []
+
+for (let i = 0; i < wordArray.length; i++) {
+
+    let word = wordArray[i]
+
+    if (word.length >= 2) {
+        let lowerCaseW = word.toLowerCase()
+        
+        let transform= lowerCaseW[0]+lowerCaseW[1].toUpperCase()+lowerCaseW.slice(2)
+
+        newword.push(transform)
+
+
+    }
+
+
+}
+console.log(newword.join(' '))
+
+
+
