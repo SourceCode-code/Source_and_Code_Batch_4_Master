a8d14b4ee0b67e357320a9ababfebb7ce986e6bf
SourceCode-code <siddhantsourceandcode@gmail.com>
2026-03-16 20:58:42 +0530

    final changes

--- SAUCE_LABS_PROJECT_2/tests/Regression_suit/Login_TEST/TC_Login_002_to_verify_the_login_functionality.spec.js
+++ SAUCE_LABS_PROJECT_2/tests/Regression_suit/Login_TEST/TC_Login_002_to_verify_the_login_functionality.spec.js
@@ -9,10 +9,11 @@ test("TC_Login_002_to_verify_the_login_functionality", async ({ browser }) => {
     let page = await Context.newPage()
 
     const Page_Title = data.TestData[0].page_title
+    const User = data.TestData[0].User
 
     await NavigationPage.visitBaseUrl(page)
     await NavigationPage.verifyLoginPageTitle(page, Page_Title)
-    await NavigationPage.loginWithCredetials(page, "standard_user")
+    await NavigationPage.loginWithCredetials(page, User)
 
     await page.pause()
 })
