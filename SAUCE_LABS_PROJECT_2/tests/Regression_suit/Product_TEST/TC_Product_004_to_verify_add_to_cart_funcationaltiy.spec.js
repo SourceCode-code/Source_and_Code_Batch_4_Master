c582dceb07d0c69eb1519e461c1c76b426a88c40
SourceCode-code <siddhantsourceandcode@gmail.com>
2026-03-25 21:03:43 +0530

    assignment 4

--- SAUCE_LABS_PROJECT_2/tests/Regression_suit/Product_TEST/TC_Product_004_to_verify_add_to_cart_funcationaltiy.spec.js
+++ SAUCE_LABS_PROJECT_2/tests/Regression_suit/Product_TEST/TC_Product_004_to_verify_add_to_cart_funcationaltiy.spec.js
@@ -18,9 +18,5 @@ test("TC_Product_004_to_verify_add_to_cart_funcationaltiy", async ({ browser }) 
     await NavigationPage.loginWithCredetials(page, User)
     await Productpage.verifyProductPageTitle(page, Product_title)
     await Productpage.verifyAddedItemToCart(page,["Sauce Labs Backpack","Sauce Labs Fleece Jacket","Test.allTheThings() T-Shirt (Red)"])
-    await page.pause()
-
-
-
 
 })
