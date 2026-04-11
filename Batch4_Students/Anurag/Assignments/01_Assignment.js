a6da0903c0a55761382d4e4075b48d9ca8ca4a24
Rasane Anurag Prashant <125312282+anuragrasane@users.noreply.github.com>
2026-04-11 13:01:27 +0530

    created new branch

---
+++ Batch4_Students/Anurag/Assignments/01_Assignment.js
@@ -0,0 +1,36 @@
+// Write a single line comment which says, comments can make code readable
+//Comments can make code readable
+
+// Write another single comment which says, Welcome to 30DaysOfJavaScript
+//Welcome to 30DaysOfJavascript
+
+// Write a multiline comment which says, comments can make code readable, easy to reuse and informative
+/*
+Comments can make code readable,easy to reuse and infomative
+*/
+
+// Declare four variables without assigning values
+ let age;
+ let firstName;
+ var lastName;
+ var companyName;
+
+
+// Declare four variables with assigned value
+
+// Declare variables to store your first name, last name, marital status, country and age in multiple lines
+
+
+// Declare variables to store your first name, last name, marital status, country and age in a single line
+
+
+// Declare two variables myAge and yourAge and assign them initial values and log to the console.
+
+
+
+// Create a variable.js file and declare variables and assign string, boolean, undefined and null data types
+
+
+
+
+// Create datatypes.js file and use the JavaScript typeof operator to check different data types. Check the data type of each variable
